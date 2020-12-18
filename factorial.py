d954f5031f35db0ca904f1fc0c6cbc9eb586c453
Michal Kaptur <kaptur.michal@gmail.com>
2020-12-18 23:49:39 +0100

    add factorial division to sets

--- factorial.py
+++ factorial.py
@@ -1,6 +1,16 @@
 def factorial(last, first=1):
     # expected n >= 0
+    first = max(first,1)
     result = 1
     for i in range(first,last+1):
         result = result * i
+    return result
+
+def partial_factorial_sets(n, sets):
+    def ceil_div(a, b):
+        return -(a // -b) #thanks SO
+    set_capacity = ceil_div(n, sets)
+    result = []
+    for i in range(0, sets):
+        result.append((i*set_capacity, (i+1)*set_capacity))
     return result
--- factorial_test.py
+++ factorial_test.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_unordered import unordered
 import factorial
 
 def test_factorial():
@@ -10,6 +11,12 @@ def test_factorial():
 
 def test_partial_factorial():
     assert factorial.factorial(10, 9) == 90
+    assert factorial.factorial(3, 0) == 6
     assert factorial.factorial(3, 1) == 6
     assert factorial.factorial(3, 2) == 6
     assert factorial.factorial(3, 3) == 3
+
+def test_partial_factorial_sets():
+    assert factorial.partial_factorial_sets(n=10, sets=1) == [(0, 10)]
+    assert factorial.partial_factorial_sets(n=10, sets=2) == unordered([(5, 10), (0, 5)])
+    # todo: uneven sets
