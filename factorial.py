b106e189d2b6a57a427247c6d57634101eb9fec8
Michal Kaptur <kaptur.michal@gmail.com>
2020-12-18 22:36:16 +0100

    add partial factorial

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
-def factorial(n):
+def factorial(end, start=1):
     # expected n >= 0
     result = 1
-    for i in range(1,n+1):
+    for i in range(start,end+1):
         result = result * i
     return result
--- factorial_test.py
+++ factorial_test.py
@@ -7,3 +7,9 @@ def test_factorial():
     assert factorial.factorial(1) == 1
     assert factorial.factorial(2) == 2
     assert factorial.factorial(3) == 6
+
+def test_partial_factorial():
+    assert factorial.factorial(10, 9) == 90
+    assert factorial.factorial(3, 1) == 6
+    assert factorial.factorial(3, 2) == 6
+    assert factorial.factorial(3, 3) == 3
