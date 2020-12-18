43b77f1e1baaba2b12dcbe9f49a321e0f89b4cd6
Michal Kaptur <kaptur.michal@gmail.com>
2020-12-18 22:37:26 +0100

    improve factorial impl arg naming

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
-def factorial(end, start=1):
+def factorial(last, first=1):
     # expected n >= 0
     result = 1
-    for i in range(start,end+1):
+    for i in range(first,last+1):
         result = result * i
     return result
