66f07d00b5980e7a1f5f0a5ba205544e41755c0f
Michal Kaptur <kaptur.michal@gmail.com>
2020-12-18 21:19:37 +0100

    actually finish the remote call and return

--- rabbit_distr.py
+++ rabbit_distr.py
@@ -1,7 +1,7 @@
 import pika
 import uuid
 
-class RcpRunner:
+class RpcRunner:
     def __init__(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = self.connection.channel()
@@ -10,15 +10,20 @@ class RcpRunner:
         self.callback_queue = result.method.queue
         self.channel.basic_consume(queue=self.callback_queue, on_message_callback=self.on_resp, auto_ack=True)
 
+        self.response = None
+
     def on_resp(self, ch, method, properties, body):
-        print("recived response %s" % body.decode())
+        if self.corr_id == properties.correlation_id:
+            self.response = body
 
     def call(self, value):
         self.corr_id = str(uuid.uuid4())
         props = pika.BasicProperties(reply_to=self.callback_queue,correlation_id=self.corr_id)
         self.channel.basic_publish(exchange='', routing_key='best_queue_ever', body=str(value), properties=props)
-        self.channel.start_consuming()
+        while not self.response:
+            self.connection.process_data_events()
+        return self.response
 
 def go():
-    runner = RcpRunner()
-    runner.call("foobar")
+    result = RpcRunner().call("foo")
+    print("got response: {}".format(result.decode()))
